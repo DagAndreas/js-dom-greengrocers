e008a6d5fdf5211aaeb7e7e14c2ab8376bca9e69
Dag Andreas <daanfofo@gmail.com>
2024-09-24 11:07:59 +0200

    adjusts price when adding to cart

--- index.js
+++ index.js
@@ -52,104 +52,111 @@ const state = {
     },
   ],
   cart: [],
-}
+};
 
-const storeList = document.querySelector(".store--item-list")
+const storeList = document.querySelector(".store--item-list");
 
-const cartList = document.querySelector(".cart--item-list")
+const cartList = document.querySelector(".cart--item-list");
 
 function renderStore() {
   state.items.forEach((item) => {
-    let newItem = document.createElement("li")
+    let newItem = document.createElement("li");
 
-    let newItemDiv = document.createElement("div")
+    let newItemDiv = document.createElement("div");
 
-    let newItemDivImage = document.createElement("img")
-    newItemDivImage.src = `assets/icons/${item.id}.svg`
+    let newItemDivImage = document.createElement("img");
+    newItemDivImage.src = `assets/icons/${item.id}.svg`;
 
-    let newItemButton = document.createElement("button")
-    newItemButton.textContent = "Add to cart"
-    newItemButton.addEventListener("click", function(){
-      onCartButtonClick(item)
-    })
+    let newItemButton = document.createElement("button");
+    newItemButton.textContent = "Add to cart";
+    newItemButton.addEventListener("click", function () {
+      onCartButtonClick(item);
+    });
 
     // add childs
-    newItemDiv.appendChild(newItemDivImage)
-    newItem.appendChild(newItemDiv)
-    newItem.appendChild(newItemButton)
-    storeList.appendChild(newItem)
-  })
+    newItemDiv.appendChild(newItemDivImage);
+    newItem.appendChild(newItemDiv);
+    newItem.appendChild(newItemButton);
+    storeList.appendChild(newItem);
+  });
 }
-renderStore()
-
+renderStore();
 
 function onCartButtonClick(item) {
-  let cartItem = document.createElement('li')
+  let cartItem = document.createElement("li");
 
-  let cartItemImage = document.createElement('img')
-  cartItemImage.classList.add('cart--item-icon')
-  cartItemImage.src = `assets/icons/${item.id}.svg`
-  cartItemImage.alt = item.name
+  let cartItemImage = document.createElement("img");
+  cartItemImage.classList.add("cart--item-icon");
+  cartItemImage.src = `assets/icons/${item.id}.svg`;
+  cartItemImage.alt = item.name;
 
-  let cartItemParagraph = document.createElement('p')
-  cartItemParagraph.textContent = item.name
+  let cartItemParagraph = document.createElement("p");
+  cartItemParagraph.textContent = item.name;
 
-  let cartItemRemoveButton = document.createElement('button')
-  cartItemRemoveButton.classList.add('quantity-btn', 'remove-btn', 'center')
-  cartItemRemoveButton.textContent = '-'
-  // TODO: Add remove-from-cart
-  cartItemRemoveButton.addEventListener("click", function(){
-    removeFromCart(cartItem)
-  })
-  
+  let cartItemRemoveButton = document.createElement("button");
+  cartItemRemoveButton.classList.add("quantity-btn", "remove-btn", "center");
+  cartItemRemoveButton.textContent = "-";
+  cartItemRemoveButton.addEventListener("click", function () {
+    removeFromCart(cartItem, item);
+  });
 
-  let cartItemSpan = document.createElement('span')
-  cartItemSpan.classList.add('quantity-text', 'center')
-  cartItemSpan.textContent = '1'
+  let cartItemSpan = document.createElement("span");
+  cartItemSpan.classList.add("quantity-text", "center");
+  cartItemSpan.textContent = "1";
 
-  let cartItemAddButton = document.createElement('button')
-  cartItemAddButton.classList.add('quantity-btn', 'add-btn', 'center')
-  cartItemAddButton.textContent = '+'
-  // TODO: Add add-to-cart button click event handler here
-  cartItemAddButton.addEventListener('click', function(){
-    addToCart(cartItem)
-  })
+  let cartItemAddButton = document.createElement("button");
+  cartItemAddButton.classList.add("quantity-btn", "add-btn", "center");
+  cartItemAddButton.textContent = "+";
+  cartItemAddButton.addEventListener("click", function () {
+    addToCart(cartItem, item);
+  });
 
   // Append children to the list item
-  cartItem.appendChild(cartItemImage)
-  cartItem.appendChild(cartItemParagraph)
-  cartItem.appendChild(cartItemRemoveButton)
-  cartItem.appendChild(cartItemSpan)
-  cartItem.appendChild(cartItemAddButton)
+  cartItem.appendChild(cartItemImage);
+  cartItem.appendChild(cartItemParagraph);
+  cartItem.appendChild(cartItemRemoveButton);
+  cartItem.appendChild(cartItemSpan);
+  cartItem.appendChild(cartItemAddButton);
 
   // Add the list item to the cart
   // TODO: check for duplicates?
-  cartList.appendChild(cartItem)
+  cartList.appendChild(cartItem);
+
+  updatePrice(item.price)
 }
 
-
-function removeFromCart(cartItem){
+function removeFromCart(cartItem, item) {
   // get amount in cart
-  const cartItemSpan = cartItem.querySelector('.quantity-text');
+  const cartItemSpan = cartItem.querySelector(".quantity-text");
   const currentAmount = parseInt(cartItemSpan.textContent, 10);
 
-  if (currentAmount > 1){
+  if (currentAmount > 1) {
     cartItemSpan.textContent = currentAmount - 1;
     return;
   }
-  
+
   // remove dom
-  cartItem.remove()
+  cartItem.remove();
+
+  updatePrice(0 - item.price);
 }
 
-function addToCart(cartItem){
+function addToCart(cartItem, item) {
   // get current amount
-  const cartItemSpan = cartItem.querySelector('.quantity-text');
+  const cartItemSpan = cartItem.querySelector(".quantity-text");
   const currentAmount = parseInt(cartItemSpan.textContent, 10);
 
   cartItemSpan.textContent = currentAmount + 1;
+
+  updatePrice(item.price);
 }
 
-function updatePrice(){
-  
+const priceSpan = document.querySelector(".total-number");
+function updatePrice(deltaPrice) {
+  const currPriceText = priceSpan.textContent.replace('£', '').trim();
+  const currPrice = parseFloat(currPriceText, 10);
+
+  const newPrice = currPrice + deltaPrice;
+
+  priceSpan.textContent = "£" + newPrice.toFixed(2);
 }
